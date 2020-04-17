394da472378d751e3f0b55ef524230df2357c084
Tony Aquilano <tony.aquilano@mobiledefense.com>
2020-04-17 10:10:23 -0400

    Remove junk

--- src/Game/ui/GameScreen/views/ResultView.tsx
+++ src/Game/ui/GameScreen/views/ResultView.tsx
@@ -22,39 +22,3 @@ export default ({result, guess}: Props) => {
 
   return <></>
 }
-
-/**
- * 
- * import React from 'react'
-import {Text, Button} from 'react-native'
-import {CheckGuessResult} from '../../../businessLogic/checkGuess'
-
-interface Props {
-  result: CheckGuessResult | null
-  guess: number
-  onResetGame: () => void
-}
-let ResultView = ({result, guess, onResetGame}: Props) => {
-  if (result === 'tooLow') {
-    return <Text>{guess} is too low.</Text>
-  }
-
-  if (result === 'tooHigh') {
-    return <Text>{guess} is too high.</Text>
-  }
-
-  if (result === 'correct') {
-    return (
-      <>
-        <Text>That's correct! 🎉</Text>
-        <Button title="New Game" onPress={onResetGame} />
-      </>
-    )
-  }
-
-  return <></>
-}
-
-export default ResultView
-
- */
